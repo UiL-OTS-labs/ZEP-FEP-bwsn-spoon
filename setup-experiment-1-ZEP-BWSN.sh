2fc1adc69da2f53c40b27db04b5da7a4c21169e6
CPA van Run <C.P.A.vanrun@uu.nl>
2016-03-07 13:46:48 +0100

    Minor changes to how particpant order is read in. Minor changes to practice items. Minor changes to what is being saved and stored

--- setup-experiment-1-ZEP-BWSN.sh
+++ setup-experiment-1-ZEP-BWSN.sh
@@ -1,35 +1,7 @@
 #!/bin/bash
-present=$(aplay -l | grep -ic 'Xonar');
-if [ $present -eq 1 ] 
-	then
-		zenity --warning --text 'Soundcards have not correctly been initiated. A reboot is recommended!'
-	exit
-fi
-
 #NOTE: configured for PHON1 computer.
 restore-default-audio-settings
 
-#Selected audiodevice for noise:
-export AUDIODEV=plughw:1,0
-
-# Play noise on a channel.
-AUDIOFILE="BrownNoise_HighPass_25Hz.wav"
-MESSAGE="Press [Enter] to start playing $AUDIOFILE and [CTRL+C] once to pause, twice to exit"
-COMMAND="echo $MESSAGE && read && play $AUDIOFILE repeat 9999"
-TERMINALTITLE="Noise Sound Generator at $AUDIODEV"
-
-# Just make sure the volume for any noise is set correctly:
-# card 0 is Xonar and used for recording / zep beeps.
-amixer -c 0 sset Master 100% on
-
-# card 1 is the onboard intel and used for playing the brown noise
-amixer -c 1 sset Master 100% on
-amixer -c 1 sset Front 100% on
-amixer -c 1 sset PCM 100% on
-
-#USE sox ('play;') to play it via the following audiodevice
-gnome-terminal --title "$TERMINALTITLE" -x bash -c "while true; do $COMMAND; done" &
-
 #start up ZEP experiment
-gnome-terminal --title "ZEP Console" -x bash -c "zep-1.6 ./bwsn/bwsn.zp;bash"
+gnome-terminal --title "ZEP Console" -x bash -c "cd bwsn; zep bwsn.zp; bash"
 
